da97af678ba678f5e49b660d7e697455373a2a3a
Surya Ghising <41846278+Suryaghising@users.noreply.github.com>
2022-09-23 11:43:14 +0545

    Update legend.dart

--- lib/src/legend.dart
+++ lib/src/legend.dart
@@ -6,44 +6,59 @@ class Legend extends StatelessWidget {
     required this.color,
     required this.style,
     required this.legendShape,
+    required this.value,
+    this.prefixWidget,
+    this.prefixText,
+    this.subtitleStyle,
     Key? key,
   }) : super(key: key);
 
   final String title;
   final Color color;
+  final String value;
+  final Widget? prefixWidget;
+  final String? prefixText;
   final TextStyle style;
+  final TextStyle? subtitleStyle;
   final BoxShape legendShape;
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        Container(
-          margin: const EdgeInsets.symmetric(vertical: 2.0),
-          height: 20.0,
-          width: 18.0,
-          decoration: BoxDecoration(
-            shape: legendShape,
-            color: color,
+        return Container(
+          margin: EdgeInsets.symmetric(vertical: 16),
+          decoration: prefixWidget == null? BoxDecoration(border: Border(left: BorderSide(color: color, width: 4))): null,
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              prefixWidget ?? SizedBox(),
+              if(prefixWidget != null) SizedBox(width: 16,),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Flexible(
+                    fit: FlexFit.loose,
+                    child: Text(
+                      title,
+                      style: style,
+                      softWrap: true,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 8.0,
+                  ),
+                  Text(
+                    '${prefixText ?? ''} $value'.trim(),
+                    style: subtitleStyle,
+                    softWrap: true,
+                  ),
+                ],),
+            ],
           ),
-        ),
-        const SizedBox(
-          width: 8.0,
-        ),
-        Flexible(
-          fit: FlexFit.loose,
-          child: Text(
-            title,
-            style: style,
-            softWrap: true,
-          ),
-        ),
-        const SizedBox(
-          width: 8.0,
-        ),
-      ],
-    );
+        );
+      // ],
+    // );
   }
 }
